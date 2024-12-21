a6fe7d14a4ae38ec73d8a548df28438df829405c
Rohan Sharma <rohankumar_23ce184@dtu.ac.in>
2024-12-21 14:54:26 +0530

    ADD PROJECT FILES

--- routes/review.js
+++ routes/review.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const {listingSchema , reviewSchema}= require("../schema.js");
-const ExpressError = require("../utils/expresserror.js");
+const ExpressError = require("../utils/ExpressError.js");
 const Listing = require('../models/listing.js');
 const Review = require('../models/review.js');
 
